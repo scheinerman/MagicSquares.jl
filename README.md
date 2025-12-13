e8a91d860e4b67c73afdbef39923553fae6f8be0
Ed Scheinerman <ers@jhu.edu>
2025-12-13 11:21:53 -0500

    First working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # MagicSquares
 
 Demonstration project using integer linear programming to create magic squares.
+
+## Optimization Formulation
+
+To find a magic square of order $n$, we use the following ILP:
+* Let $m=n(n^2+1)/2$.
+* $x_{i,j,k}$ is a 0,1-variable with $i,j\in[n]$ and $k\in[n^2]$. Indicates that the $i,j$-entry of the magic square is $k$. 
+* Constraints:
+    * Exactly one square has value $k$: $\forall k, \sum_{i,j} x_{i,j,k} = 1$ and $\forall i,j, \sum_k x_{i,j,k} = 1$. 
+    * Row sums are $m$: $\forall i, \sum_j \sum_k k x_{i,j,k} = m$. 
+    * Column sums are $m$: $\forall j, \sum_i \sum_k k x_{i,j,k} = m$. 
+    * Diagonal sums are $m$: $\sum_i \sum_k k x_{i,i,k} = m$ and $\sum_i \sum_k k x_{i,n+1-i,k} = m$.
--- src/MagicSquares.jl
+++ src/MagicSquares.jl
@@ -3,6 +3,74 @@ module MagicSquares
 using JuMP
 using ChooseOptimizer
 
-greet() = print("Hello World!")
+export magic
+
+"""
+    magic(n::Int)
+
+Create a magic square of order `n` using integer linear programming. Throws an error
+if no such magic square exists. 
+"""
+function magic(n::Int)
+    @assert n>0 "Magic square size must be positive."
+
+    MOD = Model(get_solver())
+    m = n * (n^2 + 1) ÷ 2
+
+    @variable(MOD, x[1:n, 1:n, 1:(n ^ 2)], Bin)
+
+    # The value k appears in at most one cell
+    for k in 1:(n ^ 2)
+        @constraint(MOD, sum(x[i, j, k] for i in 1:n for j in 1:n) == 1)
+    end
+
+    # The value k cannot appear in more than one cell
+    for i in 1:n
+        for j in 1:n
+            @constraint(MOD, sum(x[i, j, k] for k in 1:(n ^ 2)) == 1)
+        end
+    end
+
+    # Row sums equal m 
+    for i in 1:n
+        @constraint(MOD, sum(k*x[i, j, k] for j in 1:n for k in 1:(n ^ 2)) == m)
+    end
+
+    # Column sums equal m 
+    for j in 1:n
+        @constraint(MOD, sum(k*x[i, j, k] for i in 1:n for k in 1:(n ^ 2)) == m)
+    end
+
+    # Diagonal sums equal m 
+    @constraint(MOD, sum(k*x[i, i, k] for i in 1:n for k in 1:(n ^ 2))==m)
+    @constraint(MOD, sum(k*x[i, n + 1 - i, k] for i in 1:n for k in 1:(n ^ 2))==m)
+
+    @show n, m
+
+    optimize!(MOD)
+    t = Int(termination_status(MOD))
+
+    @show t 
+
+    if Int(t) ≠ 1 
+        error("No magic square of order $n exists")
+    end
+
+    X = Int.(round.(value.(x)))
+
+    M = zeros(Int, n, n)
+    for i in 1:n
+        for j in 1:n
+            for k in 1:(n * n)
+                if X[i, j, k] == 1
+                    M[i, j] = k
+                end
+            end
+        end
+    end
+
+
+    return M
+end
 
 end # module MagicSquares
